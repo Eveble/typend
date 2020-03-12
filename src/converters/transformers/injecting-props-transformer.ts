4b23102a82ea70521271057284bc4ea6b8728720
Adam Desivi <desivi@desivi.com>
2020-03-12 15:39:02 +0100

    feat(transformers): add InjectingPropsTransformer

---
+++ src/converters/transformers/injecting-props-transformer.ts
@@ -0,0 +1,34 @@
+import 'reflect-metadata';
+import merge from 'deepmerge';
+import { isPlainObject } from 'lodash';
+import { INJECTABLE_PROPERTIES_KEY } from '../../constants/metadata-keys';
+import { types } from '../../types';
+import { Class } from '../../patterns/class';
+
+export class InjectingPropsTransformer implements types.TypeTransformer {
+  /**
+   * Evaluates whether transformation can be done on type.
+   * @param type - Evaluated type to be transformed.
+   * @returns Returns `true` if type is instance of `Class` pattern that contains a type that has injectable properties metadata assigned, else `false`.
+   */
+  canTransform(type: types.Type): boolean {
+    return (
+      type instanceof Class &&
+      Reflect.hasOwnMetadata(INJECTABLE_PROPERTIES_KEY, type.type)
+    );
+  }
+
+  /**
+   * Transforms class type by injecting additional properties in to type's existing properties.
+   * @param classType - `Class` pattern instance as a type to be transformed.
+   * @returns Instance of `Class` pattern as a type with injected properties.
+   */
+  transform(classType: Class): Class {
+    const injectableProps: types.Type =
+      Reflect.getOwnMetadata(INJECTABLE_PROPERTIES_KEY, classType.type) || {};
+    classType.properties = merge(classType.properties, injectableProps, {
+      isMergeableObject: isPlainObject,
+    });
+    return classType;
+  }
+}
