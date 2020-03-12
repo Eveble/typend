2d14c65b936f37fbd2fc28ee04c4b132d3d19b28
Adam Desivi <desivi@desivi.com>
2020-03-12 15:37:20 +0100

    feat(converters): add UnrecognizedConverter

---
+++ test/unit/converters/tsruntime/type-converters/unrecognized-converter.test.ts
@@ -0,0 +1,34 @@
+import { expect } from 'chai';
+import { reflect } from 'tsruntime';
+import { UnrecognizedConverter } from '../../../../../src/converters/tsruntime/type-converters/unrecognized-converter';
+import { Unrecognized } from '../../../../../src/patterns/unrecognized';
+
+describe(`UnrecognizedConverter`, function() {
+  let typeConverter: UnrecognizedConverter;
+
+  before(() => {
+    typeConverter = new UnrecognizedConverter();
+  });
+
+  describe('evaluation', () => {
+    it(`returns true for passed 'object' declaration`, () => {
+      expect(typeConverter.isConvertible(reflect<object>())).to.be.true;
+    });
+  });
+
+  describe('reflection', () => {
+    it('reflects unrecognized type to instance of Unrecognized', () => {
+      expect(typeConverter.convert(reflect<object>())).to.be.instanceof(
+        Unrecognized
+      );
+    });
+  });
+
+  describe('conversion', () => {
+    it('converts unrecognized reflected type to instance of Unrecognized', () => {
+      expect(typeConverter.convert(reflect<object>())).to.be.instanceof(
+        Unrecognized
+      );
+    });
+  });
+});
