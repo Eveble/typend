bf68a926d2b2fbada04cd4b1bc2b4a0d1c7a404b
Adam Desivi <desivi@desivi.com>
2020-03-12 15:31:21 +0100

    feat(patterns): add Optional pattern

---
+++ test/unit/patterns/optional.test.ts
@@ -0,0 +1,149 @@
+import { expect } from 'chai';
+import { stubInterface } from 'ts-sinon';
+import { inspect } from 'util';
+import { Optional } from '../../../src/patterns/optional';
+import { Pattern } from '../../../src/pattern';
+import { types } from '../../../src/types';
+import { KINDS } from '../../../src/constants/literal-keys';
+import { WrapperPattern } from '../../../src/wrapper-pattern';
+
+describe(`Optional`, function() {
+  let describer: any;
+
+  beforeEach(() => {
+    describer = stubInterface<types.Describer>();
+    Pattern.setDescriber(describer);
+  });
+
+  class ParentClass {
+    public value: any;
+
+    constructor(value: any) {
+      this.value = value;
+    }
+  }
+  class ChildClass extends ParentClass {}
+
+  class MyError extends Error {}
+
+  const VALID_EXPECTATIONS: any[] = [
+    // Native types
+    String,
+    Boolean,
+    Date,
+    Number,
+    RegExp,
+    Function,
+    Symbol,
+    Map,
+    Error,
+    Object,
+    undefined,
+    null,
+    // String literal
+    'abcd',
+    // Boolean literal
+    true,
+    false,
+    // Date
+    new Date('2017-02-06T20:43:13.464Z'),
+    // Number related
+    Infinity,
+    -Infinity,
+    Math.PI,
+    -Math.PI,
+    Number.MAX_VALUE,
+    -Number.MAX_VALUE,
+    1,
+    0,
+    -1,
+    -1,
+    -2147483648,
+    2147483647,
+    // Regexp
+    new RegExp(/fail/),
+    /fail/,
+    // Function
+    (): boolean => {
+      return true;
+    },
+    // Symbol
+    Symbol('key'),
+    // Map
+    new Map([['key', 'value']]),
+    // Classes
+    ParentClass,
+    new ParentClass('test'),
+    ChildClass,
+    new ChildClass('test'),
+    // Errors
+    Error,
+    new Error(),
+    EvalError,
+    new EvalError(),
+    RangeError,
+    new RangeError(),
+    ReferenceError,
+    new ReferenceError(),
+    SyntaxError,
+    new SyntaxError(),
+    TypeError,
+    new TypeError(),
+    URIError,
+    new URIError(),
+    MyError,
+    new MyError('my-message'),
+    // Objects
+    {
+      my: 'object',
+    },
+    // Arrays
+    [],
+    [String],
+    ['abcd'],
+    [true],
+    [new Date('2017-02-06T20:43:13.464Z')],
+    [Infinity],
+    [Math.PI],
+    [Number.MAX_VALUE],
+    [0],
+    [new RegExp(/fail/)],
+    [/fail/],
+    [
+      (): boolean => {
+        return true;
+      },
+    ],
+    [Symbol('test')],
+    [new Map([['key', 'value']])],
+    [ParentClass],
+    [new ParentClass('test')],
+    [
+      {
+        my: 'object',
+      },
+    ],
+  ];
+
+  it(`extends Array`, () => {
+    expect(Optional.prototype).to.instanceof(WrapperPattern);
+  });
+
+  describe('construction', () => {
+    it('takes any expectation for construction', () => {
+      for (const expectation of VALID_EXPECTATIONS) {
+        const expectationStr = inspect(expectation);
+        describer.describe.returns(expectationStr);
+        expect(
+          new Optional(expectation),
+          `Expected expectation ${expectationStr} to be valid`
+        ).to.eql([expectation]);
+      }
+    });
+
+    it(`has assigned 'OPTIONAL' as type kind`, () => {
+      const pattern = new Optional(null);
+      expect(pattern.getKind()).to.be.equal(KINDS.OPTIONAL);
+    });
+  });
+});
