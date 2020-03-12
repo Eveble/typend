75158cb5b8e75ee223e6d22638722a9c45a86afc
Adam Desivi <desivi@desivi.com>
2020-03-12 15:24:50 +0100

    feat: add Pattern

---
+++ src/pattern.ts
@@ -0,0 +1,66 @@
+import 'reflect-metadata';
+import { types } from './types';
+import { INITIALIZER_KEY } from './constants/metadata-keys';
+
+export abstract class Pattern extends Object {
+  public static kind = '';
+
+  public static describer: types.Describer;
+
+  /**
+   * Returns for which kind pattern is created.
+   * @returns Kind represented as a string.
+   */
+  public getKind(): string {
+    return (this.constructor as types.PatternType).kind;
+  }
+
+  /**
+   * Sets describing library.
+   * @param describer - Describer library instance.
+   */
+  static setDescriber(describer: types.Describer): void {
+    this.describer = describer;
+  }
+
+  /**
+   * Returns describing library.
+   * @returns Describer library instance.
+   */
+  static getDescriber(): types.Describer {
+    return this.describer;
+  }
+
+  /**
+   * Describes value in human readable form.
+   * @param value - Value that needs to be described.
+   * @returns Human readable value described as a string.
+   */
+  public describe(value: any): string {
+    return (this.constructor as any).getDescriber().describe(value);
+  }
+
+  /**
+   * Sets as non-enumerable the initializing value for type if present on conversion.
+   * @param initializer - Initializer value provided on conversion.
+   */
+  setInitializer(initializer: any): void {
+    Reflect.defineMetadata(INITIALIZER_KEY, initializer, this);
+  }
+
+  /**
+   * Evaluates if initializing value was assigned to type.
+   * @returns Returns `true` if initializing value is set for type, else false.
+   */
+  hasInitializer(): boolean {
+    return Reflect.hasOwnMetadata(INITIALIZER_KEY, this);
+  }
+
+  /**
+   * Returns the initializing value.
+   * @returns Initializing value, else undefined.
+   */
+  getInitializer(): any | undefined {
+    return Reflect.getOwnMetadata(INITIALIZER_KEY, this);
+  }
+}
---
+++ test/unit/pattern.test.ts
@@ -0,0 +1,63 @@
+import chai, { expect } from 'chai';
+import { stubInterface } from 'ts-sinon';
+import sinonChai from 'sinon-chai';
+import { types } from '../../src/types';
+import { Pattern } from '../../src/pattern';
+
+chai.use(sinonChai);
+
+describe(`Pattern`, function() {
+  let describer: any;
+
+  beforeEach(() => {
+    describer = stubInterface<types.Describer>();
+  });
+
+  it('returns patterns type kind', () => {
+    class MyPattern extends Pattern {
+      static kind = 'my-kind';
+    }
+    expect(new MyPattern().getKind()).to.be.equal('my-kind');
+  });
+
+  it('allows to set describer on Pattern', () => {
+    Pattern.setDescriber(describer);
+    expect(Pattern.getDescriber()).to.be.equal(describer);
+  });
+
+  it('describes provided value to string', () => {
+    class MyPattern extends Pattern {}
+    MyPattern.setDescriber(describer);
+
+    const pattern = new MyPattern();
+    const val = 'my-value';
+    const valStr = `String('my-value'`;
+    describer.describe.withArgs(val).returns(valStr);
+
+    expect(pattern.describe(val)).to.be.equal(valStr);
+    expect(describer.describe).to.be.calledOnce;
+    expect(describer.describe).to.be.calledWithExactly(val);
+  });
+
+  describe('property initializers', () => {
+    class MyPattern extends Pattern {}
+
+    describe('evaluation', () => {
+      it('returns true if property initializer is set on pattern', () => {
+        const pattern = new MyPattern();
+        pattern.setInitializer('my-initializer-value');
+        expect(pattern.hasInitializer()).to.be.true;
+      });
+      it('returns false if property initializer is missing from pattern', () => {
+        const pattern = new MyPattern();
+        expect(pattern.hasInitializer()).to.be.false;
+      });
+    });
+    it('sets the property initializer on pattern', () => {
+      const pattern = new MyPattern();
+      const initializer = 'my-initializer-value';
+      pattern.setInitializer(initializer);
+      expect(pattern.getInitializer()).to.be.equal(initializer);
+    });
+  });
+});
