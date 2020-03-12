df5897f4901197e8414454c079340d3082e895f6
Adam Desivi <desivi@desivi.com>
2020-03-12 15:37:13 +0100

    feat(converters): add UnknownConverter

---
+++ test/unit/converters/tsruntime/type-converters/unknown-converter.test.ts
@@ -0,0 +1,34 @@
+import { expect } from 'chai';
+import { reflect } from 'tsruntime';
+import { UnknownConverter } from '../../../../../src/converters/tsruntime/type-converters/unknown-converter';
+import { Unknown } from '../../../../../src/patterns/unknown';
+
+describe(`UnknownConverter`, function() {
+  let typeConverter: UnknownConverter;
+
+  before(() => {
+    typeConverter = new UnknownConverter();
+  });
+
+  describe('evaluation', () => {
+    it(`returns true for passed 'unknown' declaration`, () => {
+      expect(typeConverter.isConvertible(reflect<unknown>())).to.be.true;
+    });
+  });
+
+  describe('reflection', () => {
+    it('reflects unknown type to instance of Unknown', () => {
+      expect(typeConverter.convert(reflect<unknown>())).to.be.instanceof(
+        Unknown
+      );
+    });
+  });
+
+  describe('conversion', () => {
+    it('converts unknown reflected type to instance of Unknown', () => {
+      expect(typeConverter.convert(reflect<unknown>())).to.be.instanceof(
+        Unknown
+      );
+    });
+  });
+});
