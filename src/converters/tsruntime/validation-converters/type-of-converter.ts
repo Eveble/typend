d92677d04c96f4d77cba53d6d977105c1f63e763
Adam Desivi <desivi@desivi.com>
2020-03-12 15:39:31 +0100

    feat(converters): add TypeOfConverter

---
+++ src/converters/tsruntime/validation-converters/type-of-converter.ts
@@ -0,0 +1,88 @@
+import { get } from 'lodash';
+import { Types as tsruntimeTypes } from 'tsruntime';
+import { types } from '../../../types';
+import { Class } from '../../../patterns/class';
+import {
+  VALIDATION_TYPE_KEY,
+  VALIDATION_PAYLOAD_KEY,
+  KINDS,
+} from '../../../constants/literal-keys';
+
+export class TypeOfConverter implements types.TypeConverter {
+  /**
+   * Evaluates if provided reflected type uses utility type `$TypeOf`.
+   * @param reflectedType - Reflected type.
+   * @param converter - Instance of converter.
+   * @returns Returns `true` if reflected type is a utility type `$TypeOf` and has valid class type payload, else `false`.
+   */
+  isConvertible(
+    reflectedType: tsruntimeTypes.ReflectedType,
+    converter: types.Converter
+  ): boolean {
+    if (reflectedType.kind !== 15) {
+      return false;
+    }
+    const validationType = get(
+      reflectedType,
+      `properties.${VALIDATION_TYPE_KEY.toString()}`
+    );
+    const validationPayload = get(
+      reflectedType,
+      `properties.${VALIDATION_PAYLOAD_KEY.toString()}`
+    );
+
+    const classConverter = converter.getConverter(
+      KINDS.CLASS
+    ) as types.TypeConverter;
+    return (
+      validationType?.value === KINDS.TYPE_OF &&
+      classConverter.isConvertible(validationPayload, converter)
+    );
+  }
+
+  /**
+   * Converts reflected utility type to type of class.
+   * @param reflectedType - Reflected type.
+   * @param converter - Instance of converter.
+   * @returns Returns converted class type as instance of `Class` pattern.
+   */
+  convert(
+    reflectedType: tsruntimeTypes.ReflectedType,
+    converter: types.Converter
+  ): Class {
+    const nestedReflectedType = get(
+      reflectedType,
+      `properties.${VALIDATION_PAYLOAD_KEY.toString()}`
+    );
+
+    const classConverter = converter.getConverter(
+      KINDS.CLASS
+    ) as types.TypeConverter;
+    const classType: Class = classConverter.convert(
+      nestedReflectedType,
+      converter
+    ) as Class;
+    return classType;
+  }
+
+  /**
+   * Reflects utility type to properties of class.
+   * @param reflectedType - Reflected type.
+   * @param converter - Instance of converter.
+   * @returns Returns reflected class properties as instance of an `Object` pattern.
+   */
+  reflect(
+    reflectedType: tsruntimeTypes.ReflectedType,
+    converter: types.Converter
+  ): Record<keyof any, any> {
+    const nestedReflectedType = get(
+      reflectedType,
+      `properties.${VALIDATION_PAYLOAD_KEY.toString()}`
+    );
+
+    const classConverter = converter.getConverter(
+      KINDS.CLASS
+    ) as types.TypeConverter;
+    return classConverter.reflect(nestedReflectedType, converter);
+  }
+}
---
+++ test/unit/converters/tsruntime/validation-converters/type-of-converter.test.ts
@@ -0,0 +1,97 @@
+import { expect } from 'chai';
+import { reflect } from 'tsruntime';
+import { stubInterface } from 'ts-sinon';
+import { $TypeOf } from '../../../../../src/utility-types';
+import { define } from '../../../../../src/decorators/define';
+import { TypeOfConverter } from '../../../../../src/converters/tsruntime/validation-converters/type-of-converter';
+import { types } from '../../../../../src/types';
+import { KINDS } from '../../../../../src/constants/literal-keys';
+import { InstanceOf } from '../../../../../src/patterns/instance-of';
+import { Class } from '../../../../../src/patterns/class';
+
+describe(`TypeOfConverter`, function() {
+  @define()
+  class MyClass {
+    key: string;
+  }
+
+  let converter: any;
+  let classConverter: any;
+  let typeConverter: TypeOfConverter;
+  const validPayload = { kind: 18, type: MyClass, arguments: [] };
+
+  beforeEach(() => {
+    converter = stubInterface<types.Converter>();
+    classConverter = stubInterface<types.TypeConverter>();
+    typeConverter = new TypeOfConverter();
+    converter.typeConverters = new Map();
+
+    converter.getConverter.withArgs(KINDS.CLASS).returns(classConverter);
+  });
+
+  describe('evaluation', () => {
+    it(`returns true for reflected utility type 'TypeOf'`, () => {
+      classConverter.isConvertible.withArgs(validPayload).returns(true);
+      expect(
+        typeConverter.isConvertible(reflect<$TypeOf<MyClass>>(), converter)
+      ).to.be.true;
+    });
+
+    it(`returns false for reflected utility type 'TypeOf' with passed invalid generic as non-class`, () => {
+      classConverter.isConvertible.withArgs({ kind: 8 }).returns(false);
+      classConverter.isConvertible.withArgs({ kind: 7 }).returns(false);
+      classConverter.isConvertible
+        .withArgs({ kind: 15, properties: {} })
+        .returns(false);
+
+      expect(
+        typeConverter.isConvertible(reflect<$TypeOf<true>>(), converter),
+        `Expected <$TypeOf<true>> to not be convertible`
+      ).to.be.false;
+      expect(
+        typeConverter.isConvertible(reflect<$TypeOf<false>>(), converter),
+        `Expected <$TypeOf<false>> to not be convertible`
+      ).to.be.false;
+      expect(
+        typeConverter.isConvertible(reflect<$TypeOf<{}>>(), converter),
+        `Expected <$TypeOf<{}>> to not be convertible`
+      ).to.be.false;
+    });
+
+    it(`returns false for reflected class type`, () => {
+      expect(typeConverter.isConvertible(reflect<MyClass>(), converter)).to.be
+        .false;
+    });
+
+    it(`returns false for reflected object type`, () => {
+      expect(typeConverter.isConvertible(reflect<{}>(), converter)).to.be.false;
+    });
+  });
+
+  describe('conversion', () => {
+    it('returns converted class as Class pattern isntance', () => {
+      const properties = { key: new InstanceOf(String) };
+      const classPattern = new Class(MyClass, properties);
+      classConverter.convert
+        .withArgs(validPayload, converter)
+        .returns(classPattern);
+
+      expect(
+        typeConverter.convert(reflect<$TypeOf<MyClass>>(), converter)
+      ).to.be.eql(classPattern);
+    });
+  });
+
+  describe('reflection', () => {
+    it('returns reflected class properties', () => {
+      const properties = { key: String };
+      classConverter.reflect
+        .withArgs(validPayload, converter)
+        .returns(properties);
+
+      expect(
+        typeConverter.reflect(reflect<$TypeOf<MyClass>>(), converter)
+      ).to.be.eql(properties);
+    });
+  });
+});
