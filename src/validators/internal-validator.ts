4b262669419f0df7c28028b3c1abf19bfa74bc5b
Adam Desivi <desivi@desivi.com>
2020-03-12 15:44:12 +0100

    feat(validators): add InternalValidator

---
+++ src/validators/internal-validator.ts
@@ -0,0 +1,23 @@
+import { types } from '../types';
+import { Internal } from '../patterns/internal';
+import { AnyValidator } from './any-validator';
+
+export class InternalValidator extends AnyValidator
+  implements types.PatternValidator {
+  /**
+   * Evaluates if validator can handle provided explicit pattern or implicit expectation.
+   * @param expectation - Evaluated explicit `Pattern` instance or implicit expectation.
+   * @returns Returns `true` if pattern is instance of `Internal`, else `false`.
+   */
+  public canValidate(expectation: types.Expectation): boolean {
+    return expectation instanceof Internal;
+  }
+
+  /**
+   * Validates if value matches expectation(any internal value can be valid).
+   * @returns Returns always `true` for any value.
+   */
+  public validate(): boolean {
+    return true;
+  }
+}
